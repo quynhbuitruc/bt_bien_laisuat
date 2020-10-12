e0c018271ec5d6f7d5648880098359ecfcde7687
quynhbuitruc <quynhbui270492@gmail.com>
2020-10-12 17:06:40 +0900

    đặt biến

---
+++ interest.js
@@ -0,0 +1,7 @@
+function tinhlai() {
+let tien = Number(document.getElementById("tienVay").value)
+let lSuat = Number(document.getElementById("Laisuat").value)
+let nam = Number(document.getElementById("thoiHan").value)
+let tinhtien = tien * lSuat% * nam
+
+}
