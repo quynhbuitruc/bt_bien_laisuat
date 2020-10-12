1f2af3266aa7b36e8bb4d82ecb1c19227fad024c
quynhbuitruc <quynhbui270492@gmail.com>
2020-10-12 17:09:18 +0900

    chỉnh format

--- interest.js
+++ interest.js
@@ -1,7 +1,7 @@
 function tinhlai() {
-let tien = Number(document.getElementById("tienVay").value)
-let lSuat = Number(document.getElementById("Laisuat").value)
-let nam = Number(document.getElementById("thoiHan").value)
-let tinhtien = tien * lSuat% * nam
-document.getElementById("hienthi").innerHTML = tinhtien;
+    let tien = Number(document.getElementById("tienVay").value)
+    let lSuat = Number(document.getElementById("Laisuat").value)
+    let nam = Number(document.getElementById("thoiHan").value)
+    let tinhtien = tien * (lSuat / 100) * nam;
+    document.getElementById("hienthi").innerHTML = tinhtien;
 }
